fa4d465888b01fa39a35eb7ee38e710fae51685b
mo-schmo <gh3173@wayne.edu>
2023-10-02 20:53:52 -0400

    Revert "Cache movie and restaurants in health check"
    This reverts commit 327650d7e855a1329759068b55c49dc7a2032eeb.

--- pages/api/health.ts
+++ pages/api/health.ts
@@ -1,22 +1,14 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async function handler(
+export default function handler(
     request: NextApiRequest,
     response: NextApiResponse,
 ) {
     if (request.method === "GET") {
-        cacheResults();
         response.status(200).json({
             status: "OK"
         });
         return;
     }
     return response.status(405).json({ message: "Method not supported" });
-}
-
-const cacheResults = () => {
-    // Revalidate every 10 minutes
-    const options = { next: { revalidate: 600 } }
-    fetch(`${process.env.NEXT_PUBLIC_APP_URI}/api/movie`, options);
-    fetch(`${process.env.NEXT_PUBLIC_APP_URI}/api/restaurant`, options);
 }
