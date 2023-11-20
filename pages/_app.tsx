739483dcb33a3c9fd1a3da98431f48c5f0786d34
mo-schmo <gh3173@wayne.edu>
2023-11-20 18:15:54 -0500

    Snow theme and effect

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import '../styles/snow.css';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import { AppProps } from 'next/app';
